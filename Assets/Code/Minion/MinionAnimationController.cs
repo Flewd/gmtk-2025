e0f4508ef3c6c481a78de898add4e871b069b308
Guilherme Gama <guilherme.s.gam@gmail.com>
2025-08-02 12:07:42 +0100

    Minion Clay Like Animations

--- Assets/Code/Minion/MinionAnimationController.cs
+++ Assets/Code/Minion/MinionAnimationController.cs
@@ -11,6 +11,14 @@ public class MinionAnimationController : MonoBehaviour
 
     private Vector3 deltaPos;
     private float previousOrientation;
+
+
+    [Header("Snapping Animations")]
+    public float stepFPS = 6f;
+    private float timer = 0f;
+    private float frameTime;
+    private float currentAnimatorTime = 0f;
+
     private void Awake()
     {
         lastPos = transform.position;
@@ -18,6 +26,10 @@ public class MinionAnimationController : MonoBehaviour
         previousOrientation = 1;
 
         animator = GetComponent<Animator>();
+
+        //animator.speed = 0f; // We'll drive it manually
+        frameTime = 1f / stepFPS;
+
     }
 
     // Update is called once per frame
@@ -27,12 +39,28 @@ public class MinionAnimationController : MonoBehaviour
 
         FlipToVelocity();
         Billboard();
-
+        UpdateAnimator();
 
         lastPos = transform.position;
 
+
         animator.SetFloat("Speed", deltaPos.sqrMagnitude / Time.deltaTime);
     }
+
+    void UpdateAnimator()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= frameTime)
+        {
+            timer -= frameTime;
+            currentAnimatorTime += frameTime;
+
+            // Advance animator by one "stepped" frame
+            animator.Update(frameTime);
+        }
+    }
+
 
     void FlipToVelocity()
     {
